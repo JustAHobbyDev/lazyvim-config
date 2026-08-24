ae770cfdc2569602c975041fd8a0317d7eaef637
Dan Schmidt <justahobbydev@proton.me>
2026-08-23 19:45:50 -0500

    config: swap LazyVim explorer shortcuts

---
+++ lua/plugins/explorer.lua
@@ -0,0 +1,9 @@
+return {
+  {
+    "folke/snacks.nvim",
+    keys = {
+      { "<leader>e", "<leader>fE", desc = "Explorer Snacks (cwd)", remap = true },
+      { "<leader>E", "<leader>fe", desc = "Explorer Snacks (root dir)", remap = true },
+    },
+  },
+}
