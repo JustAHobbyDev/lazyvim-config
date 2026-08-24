7ed668cbd71508c2d4a98592a8495c26a5303a1c
Dan Schmidt <justahobbydev@proton.me>
2026-08-23 20:21:54 -0500

    config: refine search and startup UI

--- lua/plugins/colorschemes.lua
+++ lua/plugins/colorschemes.lua
@@ -1,3 +1,15 @@
 return {
+  {
+    "folke/tokyonight.nvim",
+    opts = {
+      on_highlights = function(highlights, colors)
+        local search_bg = require("tokyonight.util").blend("#ffffff", 0.08, colors.bg_highlight)
+
+        for _, group in ipairs({ "Search", "CurSearch", "IncSearch" }) do
+          highlights[group] = { bg = search_bg }
+        end
+      end,
+    },
+  },
   { "rebelot/kanagawa.nvim" },
 }
--- lua/plugins/explorer.lua
+++ lua/plugins/explorer.lua
@@ -1,6 +1,21 @@
 return {
   {
     "folke/snacks.nvim",
+    opts = {
+      scroll = { enabled = false },
+    },
+    init = function()
+      vim.api.nvim_create_autocmd("VimEnter", {
+        once = true,
+        callback = function()
+          vim.schedule(function()
+            for _, picker in ipairs(Snacks.picker.get({ source = "explorer" })) do
+              picker:close()
+            end
+          end)
+        end,
+      })
+    end,
     keys = {
       { "<leader>e", "<leader>fE", desc = "Explorer Snacks (cwd)", remap = true },
       { "<leader>E", "<leader>fe", desc = "Explorer Snacks (root dir)", remap = true },
